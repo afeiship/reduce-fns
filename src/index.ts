f52519522d12300f29cbd0888e1e0e8c408e3b43
afeiship <1290657123@qq.com>
2021-11-24 23:11:46 +0800

    feat: add reverse option

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-export default (inFn0, inFn1, inFn2) => {
+export default (inFn0, inFn1, inFn2, inIsReverse?) => {
+  const method = !!inIsReverse ? 'reduceRight' : 'reduce';
   return function (...args) {
     const len = args.length;
     if (len === 0) return inFn0(args);
     if (len === 1) return inFn1(args);
-    return args.reduce(inFn2);
+    return args[method](inFn2);
   };
 };
