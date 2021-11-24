fe64e6727ade44887543f0fd8150f1f1e8e2d850
afeiship <1290657123@qq.com>
2021-11-24 23:04:06 +0800

    style: rm comments

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export default (inFn0, inFn1, inFn2) => {
   return function (...args) {
-    // const args = [].slice.call(arguments);
     const len = args.length;
     if (len === 0) return inFn0(args);
     if (len === 1) return inFn1(args);
